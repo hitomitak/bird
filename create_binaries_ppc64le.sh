2d23a0d99fbefd696a55454707e303daa475d838
hitomi <hitomi@jp.ibm.com>
2017-05-09 16:38:57 +0900

    update script for ppc64le

--- create_binaries_ppc64le.sh
+++ create_binaries_ppc64le.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+modprobe ip6_tables
+apt-get install -y autoconf flex bison gcc  build-essential libc6-dev libncurses5-dev libreadline-dev
 mkdir dist
 autoconf
 # Configure just the protocols we need, and enable the client and IPv6
-./configure  --with-protocols="bgp pipe static" --enable-ipv6=yes --enable-client=yes --enable-pthreads=yes --build=ppc64le --with-sysconfig=linux
+./configure  --with-protocols="bgp pipe static" --enable-ipv6=yes --enable-client=yes --enable-pthreads=yes --build=ppc64le --with-sysconfig=linux-v6
 make
 
 # Remove the dynmaic binaries and rerun make to create static binaries and store off the results
